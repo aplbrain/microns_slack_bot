b473553b8daf39aaa77028071670eb356b612946
nikood <dalili.nikoo@gmail.com>
2022-07-25 15:20:37 -0400

    removed unnecessary comments

--- recurring_updates.py
+++ recurring_updates.py
@@ -8,12 +8,9 @@ from time import sleep
 env_path = Path(__file__).parent.resolve() / ".env"
 load_dotenv(dotenv_path=env_path)
 
-# the channel to send updates to
-# channel_id = " C03QE4QBA3E"
-
 time = datetime.now
 
 while True:
-    if time().hour == 11 and time().minute == 59:
+    if time().hour == 11 and time().minute == 00:
         final_bot.send_scheduled_update()
     sleep(60)
